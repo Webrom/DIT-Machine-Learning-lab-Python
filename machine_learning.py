21656165574a7565a56e4b5e56b6e37793c54e16
Webrom <webrom37@gmail.com>
2016-05-25 19:33:59 +0100

    Calcul DQR

--- machine_learning.py
+++ machine_learning.py
@@ -6,11 +6,16 @@ from sklearn.feature_extraction import DictVectorizer
 import numpy as np
 
 # Import Data
-censusData = pd.read_csv("data/diabetic_data.csv", na_values=['?'])
+censusData = pd.read_csv("data/diabetic_data.csv", na_values=['?','None'])
 
 # Drop id and patient id collumns
 censusData = censusData.drop('encounter_id', axis=1)
 censusData = censusData.drop('patient_nbr', axis=1)
+# Select target label
+targetLabels = censusData['readmitted']
+# Drop the target values
+censusData = censusData.drop('readmitted', axis=1)
+
 
 # Draw plot, uncomment to use it
 """for col in censusData.columns:
@@ -34,8 +39,7 @@ censusData = censusData.drop('patient_nbr', axis=1)
         ]
         plot_url = py.plot(data, filename='data/html/' + col + ".html")"""
 
-# Select target label
-targetLabels = censusData['readmitted']
+
 
 # Take only numericals values
 numeric_dfs = censusData.select_dtypes(include=['int64'])
@@ -43,8 +47,36 @@ numeric_dfs = censusData.select_dtypes(include=['int64'])
 # Take categories values
 cat_dfs = censusData.select_dtypes(include=['object'])
 
-# Drop the target values
-cat_dfs = cat_dfs.drop('readmitted', axis=1)
+continous_dqr = numeric_dfs.describe().transpose()
+
+categorical_dqr = cat_dfs.describe(include=['O']).transpose()
+
+continous_dqr['Miss%'] = 0
+categorical_dqr['2nd Mode'] = 0
+categorical_dqr['2nd Mode freq'] = 0
+
+for col in censusData.columns:
+    val = censusData[col].isnull().sum()
+    count = censusData[col].value_counts(sort=1)
+
+    if censusData[col].dtype != np.int64:
+        categorical_dqr.ix[col, 'Miss%'] = (val/len(censusData[col]))*100
+        try:
+            categorical_dqr.ix[col, '2nd Mode'] = count.index[1]
+            categorical_dqr.ix[col, '2nd Mode freq'] = count.irow(1)
+        except Exception:
+            categorical_dqr.ix[col, '2nd Mode'] = 0
+            categorical_dqr.ix[col, '2nd Mode freq'] = 0
+
+    else:
+        continous_dqr.ix[col, 'Miss%'] = (val/len(censusData[col]))*100
+
+    # Saving the DQRs
+
+continous_dqr.to_csv("./data/DQR-ContinousFeatures.csv")
+categorical_dqr.to_csv("./data/DQR-CategoricalFeatures.csv")
+
+
 
 # transpose into array of dictionaries (one dict per instance) of feature:level pairs
 cat_dfs = cat_dfs.T.to_dict().values()
