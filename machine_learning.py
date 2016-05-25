9815b088abb3a94ced46686a8e3518fdcb30edda
Webrom <webrom37@gmail.com>
2016-05-25 17:00:49 +0100

    Read file

--- machine_learning.py
+++ machine_learning.py
@@ -1,7 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 25 17:19:05 2016
+import pandas as pd
 
-@author: root
-"""
+#Import Data
+censusData = pd.read_csv("data/diabetic_data.csv", na_values=['?'])
 
+#Select target label
+targetLabels = censusData['readmitted']
+
+#Take only numericals values
+numeric_dfs = censusData.select_dtypes(include=['int64'])
+
+#Take categories values
+cat_dfs = censusData.select_dtypes(include=['object'])
+
+#Drop the target values
+cat_dfs = cat_dfs.drop('readmitted',axis=1)
