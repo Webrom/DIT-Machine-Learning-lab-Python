dc029895279e31863475356b062555a6f41072f5
Webrom <webrom37@gmail.com>
2016-05-25 17:43:42 +0100

    Preprocessing and Cleaning the Data

--- machine_learning.py
+++ machine_learning.py
@@ -1,16 +1,33 @@
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.feature_extraction import DictVectorizer
+import numpy as np
 
-#Import Data
+# Import Data
 censusData = pd.read_csv("data/diabetic_data.csv", na_values=['?'])
 
-#Select target label
+# Select target label
 targetLabels = censusData['readmitted']
 
-#Take only numericals values
+# Take only numericals values
 numeric_dfs = censusData.select_dtypes(include=['int64'])
 
-#Take categories values
+# Drop id and patient id collumns
+numeric_dfs = numeric_dfs.drop('encounter_id', axis=1)
+numeric_dfs = numeric_dfs.drop('patient_nbr', axis=1)
+
+# Take categories values
 cat_dfs = censusData.select_dtypes(include=['object'])
 
-#Drop the target values
-cat_dfs = cat_dfs.drop('readmitted',axis=1)
+# Drop the target values
+cat_dfs = cat_dfs.drop('readmitted', axis=1)
+
+# transpose into array of dictionaries (one dict per instance) of feature:level pairs
+cat_dfs = cat_dfs.T.to_dict().values()
+
+# convert to numeric encoding
+vectorizer = DictVectorizer(sparse=False)
+vec_cat_dfs = vectorizer.fit_transform(cat_dfs)
+
+# Merge Categorical and Numeric Descriptive Features
+train_dfs = np.hstack((numeric_dfs.as_matrix(), vec_cat_dfs))
